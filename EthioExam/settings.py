f0a3525b28b91c7f0b4760a9382074464703858b
yonas <104759558+Baysasaw@users.noreply.github.com>
2025-03-11 18:57:30 +0300

    allowed_hosts

--- EthioExam/settings.py
+++ EthioExam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-$fdockhoc0=0utyiu9mxn95nt2*g+1a=2@ng&q06+za)vek9q%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['ethio-exam.onrender.com', 'www.ethio-exam.onrender.com']
 
 # Application definition
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
