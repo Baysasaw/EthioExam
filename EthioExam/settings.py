37019dee22df5a111d8cde6fb6f19975a9cd0b45
yonas <104759558+Baysasaw@users.noreply.github.com>
2025-03-17 18:36:12 +0300

    update setting

--- EthioExam/settings.py
+++ EthioExam/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$fdockhoc0=0utyiu9mxn95nt2*g+1a=2@ng&q06+za)vek9q%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ethio-exam.onrender.com', 'www.ethio-exam.onrender.com']
-# ALLOWED_HOSTS =[]
+# ALLOWED_HOSTS = ['ethio-exam.onrender.com', 'www.ethio-exam.onrender.com']
+ALLOWED_HOSTS =[]
 
 # Application definition
 
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'exam',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'EthioExam.urls'
 
@@ -119,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
