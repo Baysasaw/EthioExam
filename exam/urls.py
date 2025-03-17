16dfe09668bed41ce58c5ca528d017f6c80cd240
yonas <104759558+Baysasaw@users.noreply.github.com>
2025-03-17 16:59:20 +0300

    add a static file

--- exam/urls.py
+++ exam/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.home, name='Home'),
+    path('about/', views.aboutus, name='About-US'),
+    path('contact-as/', views.contactus, name='contact-us'),
+    path('sign-in/', views.signin, name='signin'),
+    path('sign-up', views.signup, name='sign-up')
 ]
--- exam/views.py
+++ exam/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return render(request, 'exam/index.html')
+def home(request):
+    return render(request, 'exam/Home.html')
+
+def signin(request):
+    return render(request, 'exam/signin.html')
+
+def signup(request):
+    return render(request, 'exam/sign_up.html')
+
+def contactus(request):
+    return render(request, 'exam/contactus.html')
+
+def aboutus(request):
+    return render(request, 'exam/Aboutus.html')
+
+
 
